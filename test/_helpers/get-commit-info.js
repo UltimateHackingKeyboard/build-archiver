0db3afe8141676636baa4807884b43a5bc290fe7
Kiss Róbert <ert78gb@gmail.com>
2025-05-18 12:40:38 +0200

    test: extract GH repo and sha into constants

--- test/_helpers/get-commit-info.js
+++ test/_helpers/get-commit-info.js
@@ -1,11 +1,11 @@
-import process from 'node:process'
+import { TEST_GITHUB_SHA } from '../../lib/constants.js'
 
 /**
  * @returns {GitCommitInfo}
  */
 export default function getCommitInfo() {
   return {
-    sha: process.env.GITHUB_SHA,
+    sha: TEST_GITHUB_SHA,
     message: 'commit message',
     author: {
       date: '2020-05-01T12:000.000Z',
